1f29a2160a78fbe42459e4ab9371271ef148ddf2
Pizippo <45717043+Pizippo@users.noreply.github.com>
2026-08-01 01:49:05 +0700

    Update console log to reflect new host and port

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 
 app.use((req, res, next) => serverless(req, res, next));
 app.listen(process.env.PORT || 10000, '0.0.0.0', () => {
-  console.log(`Started addon at: http://localhost:${process.env.PORT || 7000}`);
+  console.log(`Started addon at: http://any:${process.env.PORT || 10000}`);
 });
